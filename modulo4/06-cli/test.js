cf6f2ae005e19e462118e8fc0e2b889a8c265dab
Vinicius Jadiscke <vjtasso@gmail.com>
2021-01-08 17:53:03 -0300

    Write CREATE and READ functions of database

---
+++ modulo4/06-cli/test.js
@@ -0,0 +1,27 @@
+const { deepStrictEqual, ok } = require("assert");
+const database = require("./database");
+
+const DEFAULT_ITEM_CADASTRAR = {
+  nome: "Flash",
+  poder: "Speed",
+  id: 1,
+};
+
+describe("Suite de manipulacao de Herois", () => {
+  before(async () => {
+    await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+  });
+
+  it("deve pesquisar um heroi, usando arquivos", async () => {
+    const expected = DEFAULT_ITEM_CADASTRAR;
+    const [resultado] = await database.listar(expected.id);
+    deepStrictEqual(resultado, expected);
+  });
+
+  it("deve cadastrar um heroi, usando arquivos", async () => {
+    const expected = DEFAULT_ITEM_CADASTRAR;
+    const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+    const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id);
+    deepStrictEqual(actual, expected);
+  });
+});
