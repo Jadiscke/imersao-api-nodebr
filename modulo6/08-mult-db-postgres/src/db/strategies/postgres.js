c1eed91fab851f5c83238dbab60edff2c0ee8fc5
Vinicius Jadiscke <vjtasso@gmail.com>
2021-02-03 11:48:21 -0300

    Conhecendo o Sequelize

--- modulo6/08-mult-db-postgres/src/db/strategies/postgres.js
+++ modulo6/08-mult-db-postgres/src/db/strategies/postgres.js
@@ -1,9 +1,58 @@
 const ICrud = require("./interfaces/interfaceCrud");
+const Sequelize = require("sequelize");
+
 class Postgres extends ICrud {
+  #driver = null;
+  #herois = null;
   constructor() {
     super();
+    this.#connect();
   }
+  #connect() {
+    this.#driver = new Sequelize("heroes", "jadiscke", "password", {
+      host: "localhost",
+      dialect: "postgres",
+      quoteIdentifiers: false,
+      operatorsAliases: false,
+    });
+  }
+  async defineModel() {
+    this.#herois = driver.define(
+      "herois",
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          required: true,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        nome: {
+          type: Sequelize.STRING,
+          required: true,
+        },
+        poder: {
+          type: Sequelize.STRING,
+          required: true,
+        },
+      },
+      {
+        tableName: "TB_HEROIS",
+        freezeTableName: false,
+        timestamps: false,
+      }
+    );
 
+    await this.#herois.sync();
+  }
+  async isConnected() {
+    try {
+      await this.#driver.authenticate();
+      return true;
+    } catch (error) {
+      console.log("fail", error);
+      return false;
+    }
+  }
   create(item) {
     console.log("O item foi salvo no Postgres");
   }
---
+++ modulo6/08-mult-db-postgres/src/tests/postgresStrategy.test.js
@@ -0,0 +1,15 @@
+const assert = require("assert");
+const Postgres = require("../db/strategies/postgres");
+
+const Context = require("../db/strategies/base/contextStrategy");
+
+const context = new Context(new Postgres());
+
+describe("Postgres Strategy", async function () {
+  this.timeout(Infinity);
+
+  it("PostgresSQL Connection", async () => {
+    const result = await context.isConnected();
+    assert.deepStrictEqual(result, true);
+  });
+});
